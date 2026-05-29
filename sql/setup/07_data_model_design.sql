ae7d18acb32ee926ff3f3d44737458cb774caa90
Srinath <kotasrinathreddy523@gmail.com>
2026-05-29 15:44:35 +0530

    Add warehouse model design specification

---
+++ sql/setup/07_data_model_design.sql
@@ -0,0 +1,14 @@
+-- FACT_ORDERS
+-- Grain: One row per order item
+
+-- DIM_CUSTOMERS
+-- Customer dimension
+
+-- DIM_PRODUCTS
+-- Product dimension
+
+-- DIM_PAYMENTS
+-- Payment dimension
+
+-- DIM_DATE
+-- Date dimension
